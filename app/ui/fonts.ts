44fe92761847a2863d5aab94cce0429fa1eed395
Azur Kurbasic <azurkurbasic@gmail.com>
2024-01-02 20:05:51 +0100

    Added missing

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,3 +2,8 @@
  * @project nextjs-dashboard
  * @author azurkurbasic on 2. 01. 24
  */
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['700', '400'], subsets: ['latin'] })
